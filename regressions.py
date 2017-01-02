b8b464e8e09f4b45640a66bf491d31a04721bc2a
ctgk <r0735nj5058@icloud.com>
2017-01-02 16:12:03 +0900

    added RidgeRegression and changed some lines to use @

--- regressions.py
+++ regressions.py
@@ -5,14 +5,20 @@ class LinearRegression(object):
 
     def fit(self, X, t):
         self.coef = np.linalg.pinv(X).dot(t)
-        self.std = np.sqrt(np.mean((X.dot(self.coef) - t) ** 2))
+        self.var = np.mean(np.square(X @ self.coef - t))
 
     def predict(self, X):
         return X.dot(self.coef)
 
-    def predict_dist(self, X):
-        y = self.predict(X)
-        return y, np.zeros_like(y) + self.std
+
+class RidgeRegression(LinearRegression):
+
+    def __init__(self, alpha=1e-3):
+        self.alpha = alpha
+
+    def fit(self, X, t):
+        self.coef = np.linalg.inv(
+            self.alpha * np.eye(np.size(X, 1)) + X.T @ X) @ X.T @ t
 
 
 class BayesianLinearRegression(object):
@@ -24,14 +30,14 @@ class BayesianLinearRegression(object):
     def fit(self, X, t):
         self.w_var = np.linalg.inv(
             self.alpha * np.identity(np.size(X, 1))
-            + self.beta * X.T.dot(X))
-        self.w_mean = self.beta * self.w_var.dot(X.T.dot(t))
+            + self.beta * X.T @ X)
+        self.w_mean = self.beta * self.w_var @ X.T @ t
 
     def predict(self, X):
         return X.dot(self.w_mean)
 
     def predict_dist(self, X):
         y = self.predict(X)
-        y_var = 1 / self.beta + np.sum(X.dot(self.w_var) * X, axis=1)
+        y_var = 1 / self.beta + np.sum(X @ self.w_var * X, axis=1)
         y_std = np.sqrt(y_var)
         return y, y_std
