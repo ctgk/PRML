f1629fec4b843f6799c7c2ab97d58f9b4b5f810e
ctgk <r0735nj5058@icloud.com>
2017-01-02 18:35:57 +0900

    Polynomial class now supports for one-dimensional array

--- features.py
+++ features.py
@@ -9,12 +9,14 @@ class Polynomial(object):
         self.degree = degree
 
     def transform(self, x):
+        if x.ndim == 1:
+            x = x[:, None]
         x_t = x.transpose()
         features = [np.ones(len(x))]
         for degree in range(1, self.degree + 1):
             for items in itertools.combinations_with_replacement(x_t, degree):
                 features.append(functools.reduce(lambda x, y: x * y, items))
-        return np.array(features).transpose()
+        return np.asarray(features).transpose()
 
 
 class Gaussian(object):
