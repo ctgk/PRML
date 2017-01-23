ff51030d5ede4afb4cdbb781621db56e1a3d717b
cutGordianKnot <r0735nj5058@icloud.com>
2017-01-23 15:21:23 +0900

    added weight decay function

--- prml/neural_networks/losses/__init__.py
+++ prml/neural_networks/losses/__init__.py
@@ -2,9 +2,13 @@ from .gaussian_mixture_nll import GaussianMixtureNLL
 from .squared_error import SquaredError
 from .sigmoid_cross_entropy import SigmoidCrossEntropy
 from .softmax_cross_entropy import SoftmaxCrossEntropy
+from .weight_decay import WeightDecay
+
+
 __all__ = [
     "GaussianMixtureNLL",
     "SigmoidCrossEntropy",
     "SquaredError",
-    "SoftmaxCrossEntropy"
+    "SoftmaxCrossEntropy",
+    "WeightDecay"
 ]
