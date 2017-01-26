7f6636f4cc468ff0ec74da0ab5db1f5e01e159e6
cutGordianKnot <r0735nj5058@icloud.com>
2017-01-26 22:48:00 +0900

    combined fit with fit_kernel and predict with predict_dist

--- prml/kernel_methods/gaussian_process_regressor.py
+++ prml/kernel_methods/gaussian_process_regressor.py
@@ -17,33 +17,7 @@ class GaussianProcessRegressor(object):
         self.kernel = kernel
         self.beta = beta
 
-    def fit(self, X, t):
-        """
-        estimate gaussian process
-
-        Parameters
-        ----------
-        X : ndarray (sample_size, n_features)
-            input
-        t : ndarray (sample_size,)
-            corresponding target
-
-        Attributes
-        ----------
-        covariance : ndarray (sample_size, sample_size)
-            variance covariance matrix of gaussian process
-        precision : ndarray (sample_size, sample_size)
-            precision matrix of gaussian process
-        """
-        if X.ndim == 1:
-            X = X[:, None]
-        self.X = X
-        self.t = t
-        Gram = self.kernel(X, X)
-        self.covariance = Gram + np.identity(len(X)) / self.beta
-        self.precision = np.linalg.inv(self.covariance)
-
-    def fit_kernel(self, X, t, learning_rate=0.1, iter_max=100):
+    def fit(self, X, t, iter_max=0, learning_rate=0.1):
         """
         maximum likelihood estimation of parameters in kernel function
 
@@ -53,10 +27,10 @@ class GaussianProcessRegressor(object):
             input
         t : ndarray (sample_size,)
             corresponding target
+        iter_max : int
+            maximum number of iterations updating hyperparameters
         learning_rate : float
             updation coefficient
-        iter_max : int
-            maximum number of iterations
 
         Attributes
         ----------
@@ -73,14 +47,21 @@ class GaussianProcessRegressor(object):
         if X.ndim == 1:
             X = X[:, None]
         log_likelihood_list = [-np.Inf]
-        self.fit(X, t)
+        self.X = X
+        self.t = t
+        I = np.eye(len(X))
+        Gram = self.kernel(X, X)
+        self.covariance = Gram + I / self.beta
+        self.precision = np.linalg.inv(self.covariance)
         for i in range(iter_max):
             gradients = self.kernel.derivatives(X, X)
             updates = np.array(
                 [-np.trace(self.precision.dot(grad)) + t.dot(self.precision.dot(grad).dot(self.precision).dot(t)) for grad in gradients])
             for j in range(iter_max):
                 self.kernel.update_parameters(learning_rate * updates)
-                self.fit(X, t)
+                Gram = self.kernel(X, X)
+                self.covariance = Gram + I / self.beta
+                self.precision = np.linalg.inv(self.covariance)
                 log_like = self.log_likelihood()
                 if log_like > log_likelihood_list[-1]:
                     log_likelihood_list.append(log_like)
@@ -97,7 +78,7 @@ class GaussianProcessRegressor(object):
             + self.t @ self.precision @ self.t
             + len(self.t) * np.log(2 * np.pi))
 
-    def predict(self, X):
+    def predict(self, X, with_error=False):
         """
         mean of the gaussian process
 
@@ -115,6 +96,12 @@ class GaussianProcessRegressor(object):
             X = X[:, None]
         K = self.kernel(X, self.X)
         mean = K @ self.precision @ self.t
+        if with_error:
+            var = (
+                self.kernel(X, X, False)
+                + 1 / self.beta
+                - np.sum(K @ self.precision * K, axis=1))
+            return mean.ravel(), np.sqrt(var.ravel())
         return mean
 
     def predict_dist(self, X):
