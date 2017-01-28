ff9afcdbd57ac5190fbff177432e92b8d40ada8d
ctgk <r0735nj5058@icloud.com>
2017-01-28 22:40:41 +0900

    added support vector classifier

--- prml/kernel_methods/__init__.py
+++ prml/kernel_methods/__init__.py
@@ -1,8 +1,10 @@
 from .gaussian_process_classifier import GaussianProcessClassifier
 from .gaussian_process_regressor import GaussianProcessRegressor
+from .support_vector_classifier import SupportVectorClassfier
 
 
 __all__ = [
     "GaussianProcessClassifier",
-    "GaussianProcessRegressor"
+    "GaussianProcessRegressor",
+    "SupportVectorClassfier"
 ]
