5de676b0030bf509d176d15193563afc703a1b32
ctgk <r0735nj5058@icloud.com>
2017-02-05 20:15:06 +0900

    variational linear and logistic regressions

--- prml/linear_regressors/__init__.py
+++ prml/linear_regressors/__init__.py
@@ -1,10 +1,12 @@
 from .bayesian_linear_regressor import BayesianLinearRegressor
 from .linear_regressor import LinearRegressor
 from .ridge_regressor import RidgeRegressor
+from .variational_linear_regressor import VariationalLinearRegressor
 
 
 __all__ = [
     "BayesianLinearRegressor",
     "LinearRegressor",
-    "RidgeRegressor"
+    "RidgeRegressor",
+    "VariationalLinearRegressor"
 ]
