7254ec3e67c591c7ff8e447fe74c3f7def86e029
ctgk <r0735nj5058@icloud.com>
2017-02-05 15:16:54 +0900

    variational gaussian mixture model

--- prml/distributions/__init__.py
+++ prml/distributions/__init__.py
@@ -3,6 +3,7 @@ from .beta import BetaDistribution
 from .gaussian import GaussianDistribution
 from .gaussian_mixture import GaussianMixtureDistribution
 from .students_t import StudentsTDistribution
+from .variational_gaussian_mixture import VariationalGaussianMixtureDistribution
 
 
 __all__ = [
@@ -10,5 +11,6 @@ __all__ = [
     "BetaDistribution",
     "GaussianDistribution",
     "GaussianMixtureDistribution",
-    "StudentsTDistribution"
+    "StudentsTDistribution",
+    "VariationalGaussianMixtureDistribution"
 ]
