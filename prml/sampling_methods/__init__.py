3d8bc254d39cdf4c90da1ca6f30becd086ec9cc9
ctgk <r0735nj5058@icloud.com>
2017-02-07 22:47:37 +0900

    added Metropolis Hastings algorithm

--- prml/sampling_methods/__init__.py
+++ prml/sampling_methods/__init__.py
@@ -1,10 +1,12 @@
 from .metropolis import metropolis
+from .metropolis_hastings import metropolis_hastings
 from .rejection_sampling import rejection_sampling
 from .sampling_importance_resampling import sir
 
 
 __all__ = [
     "metropolis",
+    "metropolis_hastings",
     "rejection_sampling",
     "sir"
 ]
