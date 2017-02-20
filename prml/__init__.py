840eae978ac6d725d68e511a2f86a0f3c80b5848
cutGordianKnot <r0735nj5058@icloud.com>
2017-02-20 11:28:44 +0900

    included feature_extractions

--- prml/__init__.py
+++ prml/__init__.py
@@ -1,5 +1,6 @@
 from . import clusterings
 from . import distributions
+from . import feature_extractions
 from . import features
 from . import kernel_methods
 from . import kernels
@@ -11,6 +12,7 @@ from . import neural_networks
 __all__ = [
     "clusterings",
     "distributions",
+    "feature_extractions",
     "features",
     "kernel_methods",
     "kernels",
