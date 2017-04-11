906c1b05f546df92901840ad53b6f47652ce2086
ctgk <r0735nj5058@icloud.com>
2017-04-11 22:14:38 +0900

    framework of particle filtering and smoothing

---
+++ prml/markov_models/particle.py
@@ -0,0 +1,16 @@
+from .state_space_model import StateSpaceModel
+
+
+class Particle(StateSpaceModel):
+    """
+    A class to perform particle filtering or smoothing
+    """
+
+    def __init__(self):
+        pass
+
+    def filter(self):
+        pass
+
+    def smooth(self):
+        pass
