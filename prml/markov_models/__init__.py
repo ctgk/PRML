ccb0f11acd75f039e8beb5e0c9c6cbe0f2e26cd6
cutGordianKnot <r0735nj5058@icloud.com>
2017-05-20 21:21:33 +0900

    implemented particle filter, which still has some bugs

--- prml/markov_models/__init__.py
+++ prml/markov_models/__init__.py
@@ -1,10 +1,12 @@
 from .categorical_hmm import CategoricalHMM
 from .gaussian_hmm import GaussianHMM
 from .kalman import Kalman
+from .particle import Particle
 
 
 __all__ = [
     "GaussianHMM",
     "CategoricalHMM",
-    "Kalman"
+    "Kalman",
+    "Particle"
 ]
