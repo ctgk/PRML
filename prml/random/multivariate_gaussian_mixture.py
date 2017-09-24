04bb807d1aab3bd8e5b60d4a066f8bebd099acbf
ctgk <r0735nj5058@icloud.com>
2017-09-24 14:05:03 +0900

    refactored

--- prml/random/multivariate_gaussian_mixture.py
+++ prml/random/multivariate_gaussian_mixture.py
@@ -9,7 +9,7 @@ class MultivariateGaussianMixture(RandomVariable):
     """
 
     def __init__(self,
-                 n_components=3,
+                 n_components=None,
                  means=None,
                  variances=None,
                  precisions=None,
@@ -33,10 +33,13 @@ class MultivariateGaussianMixture(RandomVariable):
         assert isinstance(n_components, int)
         if means is not None:
             self.n_components = np.size(means, 0)
+            assert n_components is None
         else:
+            assert n_components is not None
             self.n_components = n_components
         self.means = means
         if variances is not None:
+            assert precisions is None
             self.variances = variances
         elif precisions is not None:
             self.precisions = precisions
@@ -44,64 +47,78 @@ class MultivariateGaussianMixture(RandomVariable):
             self.variances = None
         self.coefs = coefs
 
-    def __setattr__(self, name, value):
-        if name is "means":
-            if isinstance(value, np.ndarray):
-                assert value.ndim == 2
-                assert np.size(value, 0) == self.n_components
-                self.ndim = np.size(value, 1)
-                object.__setattr__(self, name, value)
-            else:
-                assert value is None, "means must be either np.ndarray or None"
-                object.__setattr__(self, name, None)
-        elif name is "variances":
-            if isinstance(value, (int, float)):
-                object.__setattr__(
-                    self, name,
-                    value * np.array(
-                        [np.eye(self.ndim) for _ in range(self.n_components)]
-                    )
-                )
-                object.__setattr__(self, "precisions", 1 / self.variances)
-            elif isinstance(value, np.ndarray):
-                assert value.shape == (self.n_components, self.ndim, self.ndim)
-                np.linalg.cholesky(value)
-                object.__setattr__(self, name, value)
-                object.__setattr__(self, "precisions", np.linalg.inv(value))
-            else:
-                assert value is None, (
-                    "variances must be either int, float, np.ndarray, or None"
-                )
-                object.__setattr__(self, name, None)
-        elif name is "precisions":
-            if isinstance(value, (int, float)):
-                object.__setattr__(
-                    self, name,
-                    value * np.array(
-                        [np.eye(self.ndim) for _ in range(self.n_components)]
-                    )
-                )
-                object.__setattr__(self, "variances", 1 / self.precisions)
-            elif isinstance(value, np.ndarray):
-                assert value.shape == (self.n_components, self.ndim, self.ndim)
-                np.linalg.cholesky(value)
-                object.__setattr__(self, name, value)
-                object.__setattr__(self, "variances", np.linalg.inv(value))
-            else:
-                assert value is None, (
-                    "precision must be either int, float, np.ndarray, or None"
-                )
-        elif name is "coefs":
-            if isinstance(value, np.ndarray):
-                assert value.ndim == 1
-                assert np.allclose(value.sum(), 1)
-                object.__setattr__(self, name, value)
-            else:
-                assert value is None, "coefs must be either np.ndarray or None"
-                object.__setattr__(
-                    self, name, np.ones(self.n_components) / self.n_components)
+    @property
+    def means(self):
+        return self._means
+
+    @means.setter
+    def means(self, means):
+        if isinstance(means, np.ndarray):
+            assert means.ndim == 2
+            assert np.size(means, 0) == self.n_components
+            self.ndim = np.size(means, 1)
+            self._means = means
+        elif means is None:
+            self._means = None
         else:
-            object.__setattr__(self, name, value)
+            raise TypeError("means must be either np.ndarray or None")
+
+    @property
+    def variances(self):
+        return self._variances
+
+    @variances.setter
+    def variances(self, variances):
+        if isinstance(variances, (int, float)):
+            self._variances = variances * np.array(
+                [np.eye(self.ndim) for _ in range(self.n_components)]
+            )
+            self._precisions = 1 / self._variances
+        elif isinstance(variances, np.ndarray):
+            assert variances.shape == (self.n_components, self.ndim, self.ndim)
+            np.linalg.cholesky(variances)
+            self._variances = variances
+            self._precisions = np.linalg.inv(variances)
+        elif variances is None:
+            self._variances = None
+        else:
+            raise TypeError("variances must be one of these: int, float, np.ndarray, or None")
+
+    @property
+    def precisions(self):
+        return self._precisions
+
+    @precisions.setter
+    def precisions(self, precisions):
+        if isinstance(precisions, (int, float)):
+            self._precisions = precisions * np.array(
+                [np.eye(self.ndim) for _ in range(self.n_components)]
+            )
+            self._variances = 1 / self._precisions
+        elif isinstance(precisions, np.ndarray):
+            assert precisions.shape == (self.n_components, self.ndim, self.ndim)
+            np.linalg.cholesky(precisions)
+            self._precisions = precisions
+            self._variances = np.linalg.inv(precisions)
+        elif precisions is None:
+            self._precisions = None
+        else:
+            raise TypeError("precisions must be either int, float, np.ndarray, or None")
+
+    @property
+    def coefs(self):
+        return self._coefs
+
+    @coefs.setter
+    def coefs(self, coefs):
+        if isinstance(coefs, np.ndarray):
+            assert coefs.ndim == 1
+            assert np.allclose(coefs.sum(), 1)
+            self._coefs = coefs
+        elif coefs is None:
+            self._coefs = None
+        else:
+            raise TypeError("coefs must be either np.ndarray or None")
 
     def __repr__(self):
         return (
