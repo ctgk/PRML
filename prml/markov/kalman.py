6fec3ce05370f118368da55d9b767ab8655fe56d
ctgk <r1135nj54w@gmail.com>
2018-12-22 19:38:25 +0900

    implemented maximum likelihood estimation of kalman model parameter, but something is wrong

--- prml/markov/kalman.py
+++ prml/markov/kalman.py
@@ -126,6 +126,9 @@ class Kalman(StateSpaceModel):
         return mu, cov
 
     def smooth(self):
+        """
+        bayesian update of current estimate with future observations
+        """
         mean_smoothed_next = self.hidden_mean[self.smoothed_until]
         cov_smoothed_next = self.hidden_cov[self.smoothed_until]
         cov_pred_next = self.hidden_cov_predicted[self.smoothed_until]
@@ -137,6 +140,44 @@ class Kalman(StateSpaceModel):
         mean += gain @ (mean_smoothed_next - self.system @ mean)
         cov += gain @ (cov_smoothed_next - cov_pred_next) @ gain.T
         self.smoothing_gain.insert(0, gain)
+
+    def update_parameter(self, observation_sequence):
+        """
+        maximization step of EM algorithm
+        """
+        mu0 = self.hidden_mean[1]
+        P0 = self.hidden_cov[1]
+
+        Ezn = np.asarray(self.hidden_mean)
+        Eznzn = np.asarray(self.hidden_cov) + Ezn[..., None] * Ezn[:, None, :]
+        Eznzn_1 = np.einsum("nij,nkj->nik", self.hidden_cov[2:], self.smoothing_gain[1:-1]) + Ezn[2:, :, None] * Ezn[1:-1, None, :]
+        self.system = np.linalg.solve(np.sum(Eznzn[2:], axis=0), np.sum(Eznzn_1, axis=0).T).T
+        self.cov_system = np.mean(
+            Eznzn[2:]
+            - np.einsum("ij,nkj->nik", self.system, Eznzn_1)
+            - np.einsum("nij,kj->nik", Eznzn_1, self.system)
+            + np.einsum("ij,njk,lk->nil", self.system, Eznzn[1:-1], self.system),
+            axis=0
+        )
+        self.measure = np.linalg.solve(
+            np.sum(Eznzn[1:], axis=0),
+            np.sum(np.einsum("ni,nj->nij", Ezn[1:], observation_sequence), axis=0)
+        ).T
+        self.cov_measure = np.mean(
+            np.einsum("ni,nj->nij", observation_sequence, observation_sequence)
+            - np.einsum("ij,nj,nk->nik", self.measure, Ezn[1:], observation_sequence)
+            - np.einsum("ni,nj,kj->nik", observation_sequence, Ezn[1:], self.measure)
+            + np.einsum("ij,njk,lk->nil", self.measure, Eznzn[1:], self.measure),
+            axis=0
+        )
+        return self.system, self.cov_system, self.measure, self.cov_measure, mu0, P0
+
+    def fit(self, sequence, max_iter=10):
+        for _ in range(max_iter):
+            kalman_smoother(self, sequence)
+            param = self.update_parameter(sequence)
+            self.__init__(*param)
+        return kalman_smoother(self, sequence)
 
 
 def kalman_filter(kalman:Kalman, observed_sequence:np.ndarray)->tuple:
@@ -153,7 +194,7 @@ def kalman_filter(kalman:Kalman, observed_sequence:np.ndarray)->tuple:
     Returns
     -------
     tuple ((T, Dz) np.ndarray, (T, Dz, Dz) np.ndarray)
-        seuquence of mean and covariance at each time step
+        seuquence of mean and covariance of hidden variable at each time step
     """
     for obs in observed_sequence:
         kalman.predict()
@@ -164,6 +205,23 @@ def kalman_filter(kalman:Kalman, observed_sequence:np.ndarray)->tuple:
 
 
 def kalman_smoother(kalman:Kalman, observed_sequence:np.ndarray=None):
+    """
+    perform Kalman smoothing given Kalman model (and observed sequence)
+
+    Parameters
+    ----------
+    kalman : Kalman
+        Kalman model
+    observed_sequence : (T, Dx) np.ndarray, optional
+        sequence of observation
+        run Kalman filter if given (the default is None)
+
+    Returns
+    -------
+    tuple ((T, Dz) np.ndarray, (T, Dz, Dz) np.ndarray)
+        seuqnce of mean and covariance of hidden variable at each time step
+    """
+
     if observed_sequence is not None:
         kalman_filter(kalman, observed_sequence)
     while kalman.smoothed_until != -len(kalman.hidden_mean):
