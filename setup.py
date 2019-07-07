7ecc198f1e0780903172812c76afcf4dc878cd93
ctgk <r1135nj54w@gmail.com>
2019-07-07 23:32:48 +0900

    extras_require

--- setup.py
+++ setup.py
@@ -3,11 +3,21 @@ from setuptools import setup, find_packages
 
 setup(
     name="prml",
-    version="0.0.1",
+    version="0.1.0",
     description="Collection of PRML algorithms",
     author="ctgk",
     python_requires=">=3.6",
     install_requires=["numpy", "scipy"],
+    extras_require={
+        "dev": [
+            "flake8",
+            "jupyter",
+            "matplotlib",
+            "scikit-learn",
+            "sphinx",
+            "sphinx_rtd_theme"
+        ]
+    },
     packages=find_packages(exclude=["test", "test.*"]),
     test_suite="test"
 )
